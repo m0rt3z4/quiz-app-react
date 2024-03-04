9cad560d0a2f84a6ea1a1e75660fb76499821243
Morteza Esmaeilzadeh <morteza.es.94@gmail.com>
2024-03-04 17:13:19 +0330

    housekeeping

--- src/Components/InfoForm/index.js
+++ src/Components/InfoForm/index.js
@@ -1,26 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
-import moment, { now } from 'moment'
-import {
-  Box,
-  Grid,
-  Card,
-  Typography,
-  TextField,
-  InputLabel,
-  Select,
-  MenuItem,
-  Button,
-  Divider,
-} from '@mui/material'
-import { useTrialContext } from '../../layouts/TrialLayout/context'
-import DatePicker from './DatePicker'
+import React from 'react'
+import { Box, Grid, Card } from '@mui/material'
 import Form from './Form'
 
 const InfoForm = ({ onNext }) => {
-  const [dateValue, setDateValue] = useState(moment(now))
-  const { changeTitle } = useTrialContext()
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid
@@ -36,13 +19,13 @@ const InfoForm = ({ onNext }) => {
               alignItems: 'baseline',
               justifyContent: 'center',
               minHeight: 450,
-            //   maxHeight: 530,
+              //   maxHeight: 530,
               borderRadius: '35px',
               padding: 7,
               border: '1px solid black',
             }}
           >
-            <Form onNext={onNext}/>
+            <Form onNext={onNext} />
           </Card>
         </Grid>
       </Grid>
--- src/Components/Trial2/Trial.js
+++ src/Components/Trial2/Trial.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
-import RecognitionSlide from './RecognitionSlide'
 import Exit from './Exit'
 import { TrialGrid } from '../TrialGrid/TrialGrid'
 
