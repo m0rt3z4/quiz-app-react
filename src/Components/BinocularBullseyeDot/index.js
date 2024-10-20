51b1483547fa9d6d310e40b87e5cbf303e1656cf
Morteza Esmaeilzadeh <morteza.es.94@gmail.com>
2024-10-20 14:15:34 +0330

    fixed border and bullseye color

--- src/Components/BinocularBullseyeDot/index.js
+++ src/Components/BinocularBullseyeDot/index.js
@@ -1,12 +1,13 @@
 import BULLSEYE from '../../assets/Binocular/bullseye.png'
 
-const BinocularBullseyeDot = ({ width = 100 }) => {
+const BinocularBullseyeDot = ({ width = 100, opacity = 100 }) => {
   return (
     <img
       src={BULLSEYE}
       alt="BULLSEYE"
       style={{
         width: `${width}%`,
+        opacity: `${opacity}%`,
       }}
     />
   )
