97b76f4c79630b57e3398b76e2a0e86243f509cb
Morteza Esmaeilzadeh <morteza.es.94@gmail.com>
2025-02-14 12:14:42 +0330

    binocular counter baalance fixed.
    added main trial to '/trial; route.

--- src/modules/binocularv2/createBinocularV2Params.js
+++ src/modules/binocularv2/createBinocularV2Params.js
@@ -36,3 +36,43 @@ const generateImaginationCue = (size) => {
   const red = new Array(size / 2).fill(imaginationCueTypes.RED)
   return [...red, ...green]
 }
+
+export const createBinocularV2ParamsRevamped = () => {
+  return shuffleArray([
+    ...createBinocularV2Blocks(4, imaginationCueTypes.GREEN, 'FUSED'),
+    ...createBinocularV2Blocks(4, imaginationCueTypes.RED, 'FUSED'),
+    ...createBinocularV2Blocks(1, imaginationCueTypes.RED, 'MIXED'),
+  ])
+}
+
+const createBinocularV2Blocks = (
+  numberOf8Blocks = 4,
+  imaginationCue = imaginationCueTypes.GREEN,
+  recallType = 'FUSED'
+) => {
+  let res = []
+  for (let i = 0; i < numberOf8Blocks; i++) {
+    let blockAngleArray = [-37, -30, -22.5, -11.5, 11.5, 22.5, 30, 37]
+    let imaginationCueArray = generateImaginationCue(8)
+    for (let j = 0; j < 8; j++) {
+      const angle = blockAngleArray.splice(
+        Math.floor(Math.random() * blockAngleArray.length),
+        1
+      )[0]
+
+      res.push({
+        imaginationCue:
+          recallType === 'FUSED'
+            ? imaginationCue
+            : imaginationCueArray.splice(
+                Math.floor(Math.random() * imaginationCueArray.length),
+                1
+              )[0],
+        recallType,
+        cellId: j,
+        angle,
+      })
+    }
+  }
+  return res
+}
